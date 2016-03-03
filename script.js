f29fe5eadad3d926fb54ee1ed8745cd51ee858f8
Ntombi20 <ntombi@projectcodex.co>
2016-03-03 16:11:46 +0200

    done wit highlighting my blocks

--- script.js
+++ script.js
@@ -3,28 +3,30 @@ function randomRange(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 
 }
+
 var myRandom = randomRange(1, 9);
 document.getElementById('block1').innerHTML = myRandom;
+if (myRandom >3 && myRandom<5) {
+
+	block1.classList.add("purple");
+};
+
 var myRandom2 = randomRange(1, 9);
 document.getElementById('block2').innerHTML = myRandom2;
+if (myRandom2 >3 && myRandom<5) {
+
+	block2.classList.add("purple");
+};
+
 var myRandom3 = randomRange(1, 9);
 document.getElementById('block3').innerHTML = myRandom3;
+if (myRandom3 >3 && myRandom<5) {
 
-// var matches = function(locationClass){
+	block3.classList.add("purple");
+};
 
-//  	var blocks = document.querySelector('locationClass');
-//  	block
-// }
+var displayMessage = function(text){
+    var message = document.getElementById("message");
+    message.innerHTML = text;
+};
 
-
-// var displayMessage = function(text){
-//     var message = document.getElementById("message");
-//     message.innerHTML = text;
-// };
-
-// var block = document.getElementById('div');
-// 	if (className === "block") {
-// 		block.classList.remove("block");
-// 	 	block.classList.add(block.backgroung());
-// 	};
-	
