9c3afd76463b1ad67826494ae72f8d34992b27f4
Ntombi20 <ntombi@projectcodex.co>
2016-03-03 11:50:14 +0200

    displays random number

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function randomRange(min, max) {
 
 }
 var myRandom = randomRange(1, 9);
-
 document.getElementById('block1').innerHTML = myRandom;
-document.getElementById('block2').innerHTML = myRandom;
-document.getElementById('block3').innerHTML = myRandom;
+var myRandom2 = randomRange(1, 9);
+document.getElementById('block2').innerHTML = myRandom2;
+var myRandom3 = randomRange(1, 9);
+document.getElementById('block3').innerHTML = myRandom3;
+
+// var matches = function(locationClass){
+
+//  	var blocks = document.querySelector('locationClass');
+//  	block
+// }
 
 
+// var displayMessage = function(text){
+//     var message = document.getElementById("message");
+//     message.innerHTML = text;
+// };
+
+// var block = document.getElementById('div');
+// 	if (className === "block") {
+// 		block.classList.remove("block");
+// 	 	block.classList.add(block.backgroung());
+// 	};
+	
